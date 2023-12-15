b5ec37f6053af98b4dd22d6e2f2b744b705a58b7
Kay Lin <Kay.Lin@Funcsync.com>
2023-12-15 18:23:12 +0800

    -- complete chapter 12

--- app/ui/invoices/table.tsx
+++ app/ui/invoices/table.tsx
@@ -4,13 +4,13 @@ import InvoiceStatus from '@/app/ui/invoices/status';
 import { formatDateToLocal, formatCurrency } from '@/app/lib/utils';
 import { fetchFilteredInvoices } from '@/app/lib/data';
 
-export default async function InvoicesTable({
-  query,
-  currentPage,
-}: {
-  query: string;
-  currentPage: number;
-}) {
+interface Props {
+  query: string,
+  currentPage: number,
+}
+
+export default async function InvoicesTable({query, currentPage}: Props) {
+  // 取得自己內部資料使用 server component
   const invoices = await fetchFilteredInvoices(query, currentPage);
 
   return (
